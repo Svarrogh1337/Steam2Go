ff97f428c034e11f2af614e656d68b2dfb637f3b
Hristo Hristov <me@hhristov.info>
2022-10-06 01:57:12 +0300

    ISteamUser Interface p1

--- steamapi/user.go
+++ steamapi/user.go
@@ -3,8 +3,10 @@ package steamapi
 import (
 	"context"
 	"fmt"
+	"strings"
 )
 
+// GetFriendListResponseV1 stores the data from GetFriendListV1 ( version 1 ) method  that is part of ISteamUser interface.
 type GetFriendListResponseV1 struct {
 	Friendslist struct {
 		Friends []struct {
@@ -15,6 +17,23 @@ type GetFriendListResponseV1 struct {
 	} `json:"friendslist"`
 }
 
+// GetPlayerBansResponseV1 stores the data from GetPlayerBansV1 ( version 1 ) method  that is part of ISteamUser interface.
+type GetPlayerBansResponseV1 struct {
+	Players []struct {
+		SteamID          string `json:"SteamId"`
+		CommunityBanned  bool   `json:"CommunityBanned"`
+		VACBanned        bool   `json:"VACBanned"`
+		NumberOfVACBans  int    `json:"NumberOfVACBans"`
+		DaysSinceLastBan int    `json:"DaysSinceLastBan"`
+		NumberOfGameBans int    `json:"NumberOfGameBans"`
+		EconomyBan       string `json:"EconomyBan"`
+	} `json:"players,omitempty"`
+}
+
+// GetFriendListV1 gets the specified user friend list.
+// This call has a mandatory parameter - steamid.
+// This call accepts additional parameter - Relationship.
+// Response - GetFriendListResponseV1
 func (c *Client) GetFriendListV1(ctx context.Context, steamid int, options ...RequestParameter) (*GetFriendListResponseV1, error) {
 	const (
 		apiVersion = 1
@@ -24,6 +43,34 @@ func (c *Client) GetFriendListV1(ctx context.Context, steamid int, options ...Re
 	if params := getOptionalParameters(options...).urlParams.Encode(); params != "" {
 		apiURL += "&" + params
 	}
+	err := c.get(ctx, apiURL, &resp)
+	return &resp, err
+}
+
+// GetPlayerBansV1 gets the account ban information for specified steamids.
+// This call has a mandatory parameter - steamids, passed as slice.
+// This call has no additional parameters.
+// Response - GetPlayerBansResponseV1
+func (c *Client) GetPlayerBansV1(ctx context.Context, steamids []string) (*GetPlayerBansResponseV1, error) {
+	const (
+		apiVersion = 1
+	)
+	resp := GetPlayerBansResponseV1{}
+	apiURL := fmt.Sprintf("%s/ISteamUser/GetPlayerBans/v%d?key=%s&steamids=%s", c.baseURL, apiVersion, c.apiKey, strings.Join(steamids[:], ","))
+	err := c.get(ctx, apiURL, &resp)
+	return &resp, err
+}
+
+// GetPlayerSummariesV1 gets the account ban information for specified steamids.
+// This call has a mandatory parameter - steamids, passed as slice.
+// This call has no additional parameters.
+// Response - GetPlayerBansResponseV1
+func (c *Client) GetPlayerSummariesV1(ctx context.Context, steamids []string) (*GetPlayerBansResponseV1, error) {
+	const (
+		apiVersion = 1
+	)
+	resp := GetPlayerBansResponseV1{}
+	apiURL := fmt.Sprintf("%s/ISteamUser/GetPlayerBans/v%d?key=%s&steamids=%s", c.baseURL, apiVersion, c.apiKey, strings.Join(steamids[:], ","))
 	err := c.get(ctx, apiURL, &resp)
 	return &resp, err
 }
--- steamapi/user_test.go
+++ steamapi/user_test.go
@@ -9,18 +9,26 @@ import (
 )
 
 func TestUser(t *testing.T) {
+	steamids := []string{"76561198068163231", "76561197982426067"}
+	steamids2 := []string{""}
 	key, _ := os.LookupEnv("apikey")
 	c := NewClient(key)
 	c2 := NewClient("")
 	userV1, err := c.GetFriendListV1(context.Background(), 76561198068163231, Relationship("friend"))
-	_, err2 := c2.GetFriendListV1(context.Background(), 76561198068163231)
+	assert.NotNil(t, userV1.Friendslist.Friends[0].Steamid)
+	assert.Nil(t, err)
+	_, err1 := c2.GetFriendListV1(context.Background(), 76561198068163231)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Millisecond))
 	defer cancel()
-	_, err3 := c2.GetFriendListV1(ctx, 76561198068163231)
-	_, err4 := c2.GetFriendListV1(nil, 76561198068163231)
-	assert.Nil(t, err)
-	assert.NotNil(t, userV1.Friendslist.Friends[0].Steamid)
+	assert.NotNil(t, err1)
+	_, err2 := c2.GetFriendListV1(ctx, 76561198068163231)
 	assert.NotNil(t, err2)
+	_, err3 := c2.GetFriendListV1(nil, 76561198068163231)
 	assert.NotNil(t, err3)
-	assert.NotNil(t, err4)
+	pb, err4 := c.GetPlayerBansV1(context.Background(), steamids)
+	assert.NotNil(t, pb.Players[1].EconomyBan)
+	assert.Nil(t, err4)
+	pb2, err5 := c.GetPlayerBansV1(context.Background(), steamids2)
+	assert.Empty(t, pb2.Players)
+	assert.Nil(t, err5)
 }
